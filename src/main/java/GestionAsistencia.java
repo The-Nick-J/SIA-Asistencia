4d8972ef7267a756dde269496e21a1b62be7c52d
The-Nick-J <151190011+The-Nick-J@users.noreply.github.com>
2026-08-12 18:52:37 -0400

    cree el archivo Java y un POM.xml

---
+++ src/main/java/GestionAsistencia.java
@@ -0,0 +1,10 @@
+import java.io.*;
+
+
+public class GestionAsistencia {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader leer = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("Sistema de Gestion de Asistencia");
+    }
+}
