57a5b0de752f7830b4a8aa2a9cec4d8e4b0f960d
Nicolas Jaramillo <nickjcoder@gmail.com>
2026-08-13 15:19:13 -0400

    clase curso

---
+++ src/main/java/Curso.java
@@ -0,0 +1,47 @@
+import java.util.ArrayList;
+
+public class Curso {
+    private String nombre;
+    private String codigo;
+    private String profesorJefe;
+    private ArrayList<Alumno> alumnos;
+    
+    public Curso() {
+        this.nombre = nombre;
+        this.codigo = codigo; 
+        this.alumnos = new ArrayList<>();
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public String getProfesorJefe() {
+        return profesorJefe;
+    }
+
+    public ArrayList<Alumno> getAlumnos() {
+        return alumnos;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
+    public void setProfesorJefe(String profesorJefe) {
+        this.profesorJefe = profesorJefe;
+    }
+
+    public void setAlumnos(ArrayList<Alumno> alumnos) {
+        this.alumnos = alumnos;
+    }
+   
+}
