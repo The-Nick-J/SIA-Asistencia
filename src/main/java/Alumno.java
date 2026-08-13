8d0b5afdc8041a0c7418834092449244ff9b3054
The-Nick-J <151190011+The-Nick-J@users.noreply.github.com>
2026-08-13 00:12:37 -0400

    creacion de clase alumno

---
+++ src/main/java/Alumno.java
@@ -0,0 +1,12 @@
+
+public class Alumno {
+    private String rut;
+    private String nombre;
+    private String apellido;
+    
+    public Alumno(String rut, String nombre, String apellido) {
+        this.rut = rut;
+        this.nombre = nombre;
+        this.apellido = apellido;
+    }
+}
