3a0e65bbf1b9c0e9457b4dd3cffad8af7b83d7a5
The-Nick-J <151190011+The-Nick-J@users.noreply.github.com>
2026-08-13 09:13:25 -0400

    getters y setters de alumno

--- src/main/java/Alumno.java
+++ src/main/java/Alumno.java
@@ -9,4 +9,30 @@ public class Alumno {
         this.nombre = nombre;
         this.apellido = apellido;
     }
+    
+    
+    public String getRut() {
+        return rut;
+    }
+    
+    public String getNombre() {
+        return nombre;
+    }
+    
+    public String getApellido() {
+        return apellido;
+    }
+    
+    public void setRut(String rut) {
+        this.rut = rut;
+    }
+    
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+    
 }
