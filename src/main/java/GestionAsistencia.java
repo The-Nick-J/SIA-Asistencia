17a9d63921195236c6a529fd1022b4cee597649a
Nicolas Jaramillo <nickjcoder@gmail.com>
2026-08-13 16:03:26 -0400

    clase Asistencia

--- src/main/java/GestionAsistencia.java
+++ src/main/java/GestionAsistencia.java
@@ -5,6 +5,5 @@ public class GestionAsistencia {
 
     public static void main(String[] args) throws IOException {
         BufferedReader leer = new BufferedReader(new InputStreamReader(System.in));
-        System.out.println("Sistema de Gestion de Asistencia");
     }
 }
