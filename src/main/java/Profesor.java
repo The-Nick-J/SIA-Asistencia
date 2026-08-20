9b90f068b9210cc5e23951dae64f3d827559eeb9
The-Nick-J <151190011+The-Nick-J@users.noreply.github.com>
2026-08-19 22:43:16 -0400

    Implementar SIA-6: herencia y sobreescritura

--- src/main/java/Profesor.java
+++ src/main/java/Profesor.java
@@ -1,8 +1,40 @@
 
 public class Profesor extends Persona {
-	
-	
-	public Profesor(String rut, String nombre, String apellido) {
-		super(rut,nombre,apellido);
-	}
+
+    private String asignatura;
+
+    public Profesor(String rut, String nombre, String apellido, String asignatura) {
+        super(rut, nombre, apellido);
+        this.asignatura = asignatura;
+    }
+    
+    public String getAsignatura() {
+        return this.asignatura;
+    }
+    
+    public void setAsignatura(String asignatura) {
+        this.asignatura = asignatura;
+    }
+    
+    
+    @Override
+    public void mostrarResumen() {
+    	String nombre = this.getNombre();
+    	String rut = this.getRut();
+    	String apellido = this.getApellido();
+    	
+    	if(this.asignatura != null) {
+    		System.out.println("-----Datos del Profesor-----");
+    		System.out.println("RUT: " + rut);
+    		System.out.println("Nombre: " + nombre);
+    		System.out.println("Apellido: " + apellido);
+    		System.out.println("Asignatura: " + this.getAsignatura());
+    	} else {
+    		System.out.println("-----Datos del Profesor-----");
+    		System.out.println("RUT: " + rut);
+    		System.out.println("Nombre: " + nombre);
+    		System.out.println("Apellido: " + apellido);
+    		System.out.println("Asignatura: Sin Asignatura Asignada");
+    	}
+    }
 }
