48be8252350f1ba623ad976e311a24f3ad61ae92
Alessandro <alessandror37@gmail.com>
2026-08-23 02:59:57 -0400

    Rework GestionRegistroAsistencia ArrayList->HashMap

--- src/main/java/GestionRegistroAsistencia.java
+++ src/main/java/GestionRegistroAsistencia.java
@@ -2,58 +2,78 @@ import java.util.*;
 import java.time.*;
 
 public class GestionRegistroAsistencia{
-    private ArrayList<Asistencia> registrosAsistencia;
     
-    //el arraylist se declara en atributos y se inicializa en el constructor
+    /*Este HashMap usa de key el rut del alumno, y su value es un ArrayList
+    que contendra todas las asistencias correspondiente a un alumno
+    */
+    private HashMap<String,ArrayList<Asistencia>> registrosAsistencia;
+    
+    //el HashMap se declara en atributos y se inicializa en el constructor
     public GestionRegistroAsistencia(){
-        this.registrosAsistencia = new ArrayList<Asistencia>();
+        this.registrosAsistencia = new HashMap<>();
     }
     
-    public void agregarRegistroAsistencia(Asistencia nuevaAsistencia){
-        for(Asistencia asistencia : registrosAsistencia){
-            boolean alumno = asistencia.getAlumno().getRut().equals(nuevaAsistencia.getAlumno().getRut());
-            boolean fecha = asistencia.getFecha().equals(nuevaAsistencia.getFecha());
-            
-            if(alumno && fecha){
-                System.out.println("ERROR: El alumno ya tiene una asistencia registra en esa fecha");
-                return;
+    /*Agrega una asistencia al alumno correspondiente, el metodo se encarga
+    de revisar si ya hay asistencia para ese dia
+    */
+    public boolean agregarRegistroAsistencia(Asistencia nuevaAsistencia){
+        String rut = nuevaAsistencia.getAlumno().getRut();
+        
+        if(!registrosAsistencia.containsKey(rut)){
+             registrosAsistencia.put(rut, new ArrayList<>());
+        }
+        
+        ArrayList<Asistencia> asistenciaAlumno = registrosAsistencia.get(rut);
+        for(Asistencia asistencia : asistenciaAlumno){
+            if(asistencia.getFecha().equals(nuevaAsistencia.getFecha())){
+                System.out.println("ERROR: El alumno ya tiene una asistencia registrada en esa fecha");
+                return false;
             }
         }
-        registrosAsistencia.add(nuevaAsistencia);
+        asistenciaAlumno.add(nuevaAsistencia);
+        return true;
     }
    
-    //metodo para buscar por alumno, utiliza sobrecarga
-    public Asistencia buscarRegistroAsistencia(Alumno alumno){
-        for(Asistencia asistencia : registrosAsistencia){
-            if(asistencia.getAlumno().equals(alumno)){
-                return asistencia;
-            }
+    //Retorna un ArrayList de todas las asistencias registradas de un alumno
+    public ArrayList<Asistencia> buscarRegistroAsistencia(Alumno alumno){
+        if(alumno != null && registrosAsistencia.containsKey(alumno.getRut())){
+            return registrosAsistencia.get(alumno.getRut());
         }
         return null;
     }
-    //metodo para buscar por fecha, utiliza sobrecarga
-    public Asistencia buscarRegistroAsistencia(LocalDate fecha){
-        for(Asistencia asistencia : registrosAsistencia){
-            if(asistencia.getFecha().equals(fecha)){
-                return asistencia;
+    //Retorna un ArrayList de todas las asistencias registradas en una fecha
+    public ArrayList<Asistencia> buscarRegistroAsistencia(LocalDate fecha){
+        ArrayList<Asistencia> asistenciasFecha = new ArrayList<>();
+        for(ArrayList<Asistencia> asistenciasAlumno : registrosAsistencia.values()){
+            for(Asistencia asistencia : asistenciasAlumno){
+                if(asistencia.getFecha().equals(fecha)){
+                    asistenciasFecha.add(asistencia);
+                }
             }
         }
-        return null;
+        return asistenciasFecha;
     }
     //print las asistencias del arraylist y sus datos, dado que asistencia no tiene metodo mostrar
     public void mostrarRegistros(){
-        for(Asistencia asistencia : registrosAsistencia){
-            System.out.println("Fecha: " + asistencia.getFecha());
-            System.out.println("Alumno: " + asistencia.getAlumno());
-            if(asistencia.isPresente()){
-                System.out.println("Estado: Presente");
-            } else {
-                System.out.println("Estado: No presente");
-            }
-            if(asistencia.isRetirado()){
-                System.out.println("Estado: Retirado");
-            } else {
-                System.out.println("Estado: No retirado");
+        if(registrosAsistencia.isEmpty()){
+            System.out.println("No hay registros de asistencia en el sistema.");
+            return;
+        }
+        for(ArrayList<Asistencia> asistenciasAlumno : registrosAsistencia.values()){   
+            for(Asistencia asistencia : asistenciasAlumno){
+                System.out.println("Fecha: " + asistencia.getFecha());
+                System.out.println("Alumno: " + asistencia.getAlumno().getNombre() +" "+asistencia.getAlumno().getApellido());
+                if(asistencia.isPresente()){
+                    System.out.println("Estado: Presente");
+                } else {
+                    System.out.println("Estado: No presente");
+                }
+                if(asistencia.isRetirado()){
+                    System.out.println("Estado: Retirado");
+                } else {
+                    System.out.println("Estado: No retirado");
+                }
+                System.out.println("\n--------------\n");
             }
         }
     }
