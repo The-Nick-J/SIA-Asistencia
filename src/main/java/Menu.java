a37074f5787196d0656a3f1597c7bed42bfa1c36
The-Nick-J <151190011+The-Nick-J@users.noreply.github.com>
2026-08-29 20:37:08 -0400

    AVANCE DE MENU INTENTO 1

--- src/main/java/Menu.java
+++ src/main/java/Menu.java
@@ -1,16 +1,29 @@
+
 import java.io.BufferedReader;
 import java.io.IOException;
 
 public class Menu {
-    
+
     private final BufferedReader leer;
     private final MenuPrincipal menuPrincipal;
+    private final GestionCursos gestionCursos;
+    private final GestionAlumnos gestionAlumnos;
+    private final GestionRegistroAsistencia gestionRegistroAsistencia;
 
-    public Menu(BufferedReader leer, GestionCursos gestionCursos, GestionAlumnos gestionAlumnos,GestionRegistroAsistencia gestionRegistroAsistencia){
+    public Menu(
+            BufferedReader leer,
+            GestionCursos gestionCursos,
+            GestionAlumnos gestionAlumnos,
+            GestionRegistroAsistencia gestionRegistroAsistencia) {
+
         this.leer = leer;
-        this.menuPrincipal = new MenuPrincipal(leer, gestionCursos, gestionAlumnos,gestionRegistroAsistencia);
+        this.gestionCursos = gestionCursos;
+        this.gestionAlumnos = gestionAlumnos;
+        this.gestionRegistroAsistencia = gestionRegistroAsistencia;
+
+        this.menuPrincipal = new MenuPrincipal(leer, gestionCursos, gestionAlumnos, gestionRegistroAsistencia);
     }
-    
+
     public void mostrarMenu() throws IOException {
         int opcion;
         do {
@@ -18,15 +31,20 @@ public class Menu {
             System.out.println("1. Usar consola");
             System.out.println("2. Usar ventana");
             System.out.println("3. Salir");
-            
+
             opcion = Integer.parseInt(leer.readLine());
-            
-            if(opcion == 1){
+
+            if (opcion == 1) {
                 menuPrincipal.mostrarMenu();
-            } else if (opcion == 2){
-                System.out.println("VENTANA WORK IN PROGRESS");
-                //TO DO: ACA VA LA LLAMADA A LA VENTANA
-            } else if (opcion != 3){
+            } else if (opcion == 2) {
+                VentanaPrincipal ventanaPrincipal = new VentanaPrincipal(
+                        gestionCursos,
+                        gestionAlumnos,
+                        gestionRegistroAsistencia
+                );
+                
+                ventanaPrincipal.setVisible(true);
+            } else if (opcion != 3) {
                 System.out.println("Error: Opcion invalida");
             }
         } while (opcion != 3);
