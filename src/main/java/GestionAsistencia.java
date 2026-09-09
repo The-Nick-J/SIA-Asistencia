eef6ac767ee1e1ac5df42bf77a183e8b56a746ff
Alessandro Reginato <alessandror37@gmail.com>
2026-09-08 22:03:21 -0300

    Implementado SIA-11: Carga y persistencia de datos con CSV

--- src/main/java/GestionAsistencia.java
+++ src/main/java/GestionAsistencia.java
@@ -11,6 +11,17 @@ public class GestionAsistencia {
         GestionAlumnos gestionAlumnos = new GestionAlumnos(gestionCursos);
         GestionRegistroAsistencia gestionRegistroAsistencia = new GestionRegistroAsistencia();
         
+        CSV csv = new CSV(gestionCursos, gestionAlumnos, gestionRegistroAsistencia);
+        csv.cargarTodo();
+        
+        Runtime.getRuntime().addShutdownHook(new Thread (() -> {
+            try {
+                csv.guardarTodo();
+            } catch (IOException e) {
+                System.err.println("Error al guardar los datos en CSV: " + e.getMessage());
+            }
+        }));
+        
         Menu menu = new Menu(leer, gestionCursos, gestionAlumnos,gestionRegistroAsistencia);
         menu.mostrarMenu();
         
